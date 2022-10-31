2f4b4eee65c901bbd71843e8b05adf8dc06ce05c
David Tewkesbury <tewkesburyd@hotmail.com>
2022-10-31 11:12:02 -0600

    commit tree 384e86b9c19af13c299d0e31b689f56ae1459cbc

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = 'David';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let firstLetter = currentUser[0];
+const shortGreeting = `Welcome, ${firstLetter}!`
